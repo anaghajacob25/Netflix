90041708924deab012fa027aeb9a14151c705a72
anaghajacob25 <anaghamariajacob@gmail.com>
2024-04-19 15:33:53 +0530

    final commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+
+import './App.css'
+import Navbar from './components/Navbar'
+import Cover from './components/Cover'
+import Row from './components/Row'
+import instance from './instance'
+import requests from './request'
+
+
+function App() {
+ 
+
+  return (
+    <>
+     <Navbar/>
+     <Cover fetchUrl={requests.fetchNetflixOriginals}/>
+     <Row isPoster='true' title='Trending Movies' fetchUrl={requests.fetchTrending}/> 
+     <Row title='netflixOriginals' fetchUrl={requests.fetchNetflixOriginals}/>
+     <Row title='Top Rated' fetchUrl={requests.fetchTopRated}/>
+     <Row title='Action' fetchUrl={requests.fetchActionMovies}/>
+     <Row title='Comedy' fetchUrl={requests.fetchComedyMovies}/>
+     <Row title='Horror' fetchUrl={requests.fetchHorrorMovies}/>
+     <Row title='Romance' fetchUrl={requests.fetchRomanceMovies}/>
+     <Row title='Documentaries' fetchUrl={requests.fetchDocumentaries}/>
+   
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Cover.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react'
+import './Cover.css'
+import instance from '../instance';
+
+function Cover({fetchUrl}) {
+  console.log(fetchUrl);
+
+  const base_url="https://image.tmdb.org/t/p/original/";
+  // to store data
+
+  const[movie,setMovie]=useState({})
+
+  const fetchData=async()=>{
+  const response=await instance.get(fetchUrl)
+
+  // console.log(response.data.results[Math.floor(Math.random()*response.data.results.length-1)]);
+  setMovie(response.data.results[Math.floor(Math.random()*response.data.results.length-1)])
+  }
+
+  console.log(movie);
+
+  useEffect(()=>{
+    fetchData()
+  },[])
+
+  return (
+   <div style={{height:'560px', backgroundImage:`url(${base_url}${movie.backdrop_path})`,backgroundSize:'cover',backgroundAttachment:'fixed'}}>
+    <div className="banner-details">
+      <h1>{movie.name}</h1>
+      <button className='btn btn-danger'>Play</button>
+      <button className='btn btn-outline-light ms-4'>More Info</button>
+      <h5>{movie.overview?.slice(0,200)}...</h5>
+    </div>
+   </div>
+  )
+}
+
+export default Cover
